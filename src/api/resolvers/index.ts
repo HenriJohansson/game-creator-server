eb01499c10159976c7ddc3c9e769f32660611ca3
Henri Johansson <henri.mm.johansson@gmail.com>
2023-10-06 11:23:39 +0300

    forgot to add resolver to index.ts

--- src/api/resolvers/index.ts
+++ src/api/resolvers/index.ts
@@ -2,10 +2,12 @@ import animalResolver from './animalResolver';
 import speciesResolver from './speciesResolver';
 import categoryResolver from './categoryResolver';
 import userResolver from './userResolver';
+import markerResolver from './markerResolver';
 
 export default [
   categoryResolver,
   speciesResolver,
   animalResolver,
   userResolver,
+  markerResolver,
 ];
